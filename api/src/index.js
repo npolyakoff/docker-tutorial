f19c5910a7a5165bd994afc68528695a45498b29
Поляков Никита Игоревич <nipolya1@www.mts.ru>
2020-09-10 23:48:38 +0300

    environment variables

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT;
+const host = process.env.HOST
 
 app.get('/test', (req, res) => {
     res.send('Api server works fine!')
 })
+app.get('*', (req, res) => {
+    res.send(`Hello on port ${port}`)
+})
 
-app.listen(3000, () => {
-    console.log('Api server started')
+app.listen(port, () => {
+    console.log(`Api server started on port ${port}`)
+    console.log (`Host is ${host}`)
 })
