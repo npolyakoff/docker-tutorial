81ef3ef5464bdf79fd26826879d702465f15d83f
Поляков Никита Игоревич <nipolya1@www.mts.ru>
2020-09-11 21:19:07 +0300

    database

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { connectDb } = require('./helpers/db');
+const { port, host, db } = require('./configuration');
+
 const app = express();
-const port = process.env.PORT;
-const host = process.env.HOST
+
+
+const startServer = () => {
+    app.listen(port, () => {
+        console.log(`Api server started on port ${port}`)
+        console.log (`Host is ${host}`)
+        console.log(`Database connection: ${db}`)
+
+        // Post.find(function (err, posts) {
+        //     if (err) return console.error(err);
+        //     console.log(posts);
+        //   })
+
+        const post = new Post({ name: 'Silence' });
+        post.save(function (err, savedPost) {
+            if (err) return console.error(err);
+            console.log(savedPost)
+        });
+    })
+}
+
+const postSchema = new mongoose.Schema({
+    name: String
+});
+
+const Post = mongoose.model('Post', postSchema) 
 
 app.get('/test', (req, res) => {
     res.send('Api server works fine!')
@@ -10,7 +38,7 @@ app.get('*', (req, res) => {
     res.send(`Hello on port ${port}`)
 })
 
-app.listen(port, () => {
-    console.log(`Api server started on port ${port}`)
-    console.log (`Host is ${host}`)
-})
+connectDb()
+    .on('error', console.log)
+    .on('disconnected', connectDb)
+    .once('open', startServer)
